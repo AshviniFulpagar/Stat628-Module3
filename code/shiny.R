2a51e543d9c029c7a8a829e02fe161e470133b19
qingchuan-ji <qji5@wisc.com>
2020-12-01 01:47:39 +0800

    Shiny app code, this is th basic verison of our shiny app. It can show the characteristics of Madison data very well.

---
+++ code/shiny.R
@@ -0,0 +1,83 @@
+rm(list=ls())
+setwd("C:/Users/karlc/Desktop/Stat628-Module3")
+#Using package
+library(leaflet)
+library(shiny)
+library(dplyr)
+#Loading required information
+business_chinese<-read.csv("./clean_data/chinese/business_filter.csv")
+review = read.csv("./clean_data/Chinese/review_Chinese.csv")
+business_chinese<-business_chinese[business_chinese$city=="Madison",]
+id<-business_chinese$business_id
+name<-business_chinese$name
+state<-business_chinese$state
+lat<-business_chinese$latitude
+lng<-business_chinese$longitude
+star<-business_chinese$stars
+categories<-business_chinese$categories
+count<-business_chinese$review_count
+level<-c()
+for(i in 1:length(business_chinese$business_id)){
+  if(star[i]>=4){level[i]="High Stars"}
+  else if(star[i]>=3){level[i]="Medium"}
+  else {level[i]="Bad"}
+}
+level
+business_chinese<-mutate(business_chinese,level)
+business_chinese$level
+plot(density(star),main="Chinese food Yelp Stars Distribution in Madison",xlab="Yelp Stars")
+
+
+##User interface 
+
+ui <- navbarPage("Yelp data analysis",id="main",
+                 tabPanel("Yelp Map",leafletOutput("mymap",height=900,width="100%")),
+                 tabPanel("Yelp Star in the City",textOutput("Click_text")),
+                 
+                 tabPanel("GUIDE",includeMarkdown("./clean_data/chinese/README2.md"))
+)
+
+server<-shinyServer(function(input, output) {
+  
+  
+  # new column for the popup label
+  
+  business_chinese <- mutate(business_chinese, cntnt=paste0('<strong>Name: </strong>',name,
+                                                            '<br><strong>Category:</strong> ',categories,                 
+                                                            '<br><strong>City:</strong> ', city,
+                                                            '<br><strong>Star:</strong> ', stars,
+                                                            '<br><strong>Review Count:</strong> ',review_count)) 
+  
+  # create a color paletter for category type in the data file
+  
+  pal <- colorFactor(pal = c("#1b9e75", "#d95f02", "#7570b3"), domain = c("High Stars", "Medium", "Bad"))
+  
+  # create the leaflet map  
+  output$mymap <- renderLeaflet({
+    leaflet(business_chinese) %>% 
+      clearShapes() %>%
+      addTiles() %>%
+      addCircleMarkers(data = business_chinese, lat =  ~latitude, lng =~longitude, 
+                       radius = 7, popup = ~as.character(cntnt), 
+                       color = ~pal(level), layerId<- ~name,
+                       stroke = FALSE, fillOpacity = 1)%>%
+      addLegend(title="Yelp Star Level",pal=pal, values=business_chinese$level,opacity=1,position="topleft")%>%
+      addEasyButton(easyButton(
+        icon="fa-crosshairs", title="ME",
+        onClick=JS("function(btn, map){ map.locate({setView: true}); }")))
+  })
+  
+  observeEvent(input$mymap_marker_click, { # update the location selectInput on map clicks
+    p <- input$mymap_marker_click
+    output$Click_text<-renderText({
+      
+      business_chinese$stars[business_chinese$name==p$id]
+      
+    })
+  })
+})
+
+shinyApp(ui=ui, server=server)
+
+
+
